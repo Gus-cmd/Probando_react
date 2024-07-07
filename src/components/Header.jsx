18028d36dd35249e79a501f1f092fabc90cf8842
Gus-cmd <ghuachaca25@gmail.com>
2024-07-06 20:36:44 -0500

    i'm traying this

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import { Navbar, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
-import { FaShoppingCart } from 'react-icons/fa';
+import React from "react";
+import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="#home">GusShop | Tech</Navbar.Brand>
+        <Navbar.Brand href="#home">GusShop| tech</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#Laptops">Laptops</Nav.Link>
-            <Nav.Link href="#Accesorios">Accesorios</Nav.Link>
-            <Nav.Link href="#">Iniciar Sesion</Nav.Link>
+            <Nav.Link href="#laptops">Laptops</Nav.Link>
+            <Nav.Link href="#accesorios">Accesorios</Nav.Link>
           </Nav>
-          <Form className="d-flex ms-auto search-form">
+          <Form className="d-flex">
             <FormControl
               type="search"
-              placeholder="Buscar productos"
-              className="me-2"
-              aria-label="Buscar"
+              placeholder="Search"
+              className="mr-2"
+              aria-label="Search"
             />
-            <Button variant="outline-light">Buscar</Button>
+            <Button variant="outline-light">Search</Button>
           </Form>
-          <Nav className="ms-auto">
-            <Nav.Link href="#carrito" className="d-flex align-items-center">
-              <FaShoppingCart size={24} />
+          <Nav>
+            <Nav.Link href="#">
+              <FaShoppingCart />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
@@ -35,5 +34,6 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
